2fac3d4fa316664cc5c1f799e98b6bf8264c524d
Paul Hayes <fofr@users.noreply.github.com>
2022-02-02 14:21:26 +0000

    Move dqt methods into utils file

--- app/wizards/find-your-trn.js
+++ app/wizards/find-your-trn.js
@@ -1,4 +1,5 @@
 import { wizard } from 'govuk-prototype-rig'
+import { userMatchesDQTRecord } from '../utils/dqt.js'
 
 export function trnWizardPaths (req) {
   const paths = [
@@ -19,43 +20,6 @@ export function trnWizardPaths (req) {
   ]
 
   return wizard.nextAndBackPaths(paths, req)
-}
-
-const emailAddressMatchesDQTRecord = (data) => {
-  return data['email-address'] === data.dqt_record['email-address']
-}
-
-const nameMatchesDQTRecord = (data) => {
-  return data['first-name'] === data.dqt_record['first-name'] &&
-    data['last-name'] === data.dqt_record['last-name']
-}
-
-const dobMatchesDQTRecord = (data) => {
-  return data['date-of-birth'] &&
-    parseInt(data['date-of-birth'].day) === data.dqt_record['date-of-birth'].day &&
-    parseInt(data['date-of-birth'].month) === data.dqt_record['date-of-birth'].month &&
-    parseInt(data['date-of-birth'].year) === data.dqt_record['date-of-birth'].year
-}
-
-const ninoMatchesDQTRecord = (data) => {
-  return data['national-insurance-number'] === data.dqt_record['national-insurance-number']
-}
-
-const ittProviderMatchesDQTRecord = (data) => {
-  return (data['itt-provider']) === data.dqt_record['itt-provider']
-}
-
-const numberOfMatchingFieldsAgainstDQTRecord = (data) => {
-  let count = 0
-  if (nameMatchesDQTRecord(data)) count++
-  if (dobMatchesDQTRecord(data)) count++
-  if (ninoMatchesDQTRecord(data)) count++
-  if (ittProviderMatchesDQTRecord(data)) count++
-  return count
-}
-
-const userMatchesDQTRecord = (data) => {
-  return (emailAddressMatchesDQTRecord(data)) || numberOfMatchingFieldsAgainstDQTRecord(data) >= 3
 }
 
 export function trnWizardForks (req) {
