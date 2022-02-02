ea757c298fd2e36d37b2151386cf38c7f1a8207e
Paul Hayes <fofr@users.noreply.github.com>
2022-02-02 13:24:43 +0000

    Split example and TRN routes and wizards into separate files

--- app/routes.js
+++ app/routes.js
@@ -1,54 +1,9 @@
 import express from 'express'
-import { exampleWizardPaths, exampleWizardForks, trnWizardPaths, trnWizardForks } from './wizards.js'
+import { exampleWizardRoutes } from './routes/example-wizard.js'
+import { findYourTrnRoutes } from './routes/find-your-trn.js'
 
 const router = express.Router()
-
-/**
- * Example routes to demonstrate using wizard helper.
- */
-router.get('/examples/wizard', (req, res) => {
-  res.render('examples/wizard/index', {
-    paths: exampleWizardPaths(req)
-  })
-})
-
-router.get('/examples/wizard/:view', (req, res) => {
-  const { view } = req.params
-  const views = [
-    'check-answers',
-    'confirm',
-    'england',
-    'name',
-    'nationality',
-    'where-do-you-live'
-  ]
-
-  if (views.includes(view)) {
-    res.render(`examples/wizard/${view}`, {
-      paths: exampleWizardPaths(req)
-    })
-  }
-})
-
-router.post('/examples/wizard/:view?', (req, res) => {
-  const fork = exampleWizardForks(req)
-  const paths = exampleWizardPaths(req)
-  fork ? res.redirect(fork) : res.redirect(paths.next)
-})
-
-/**
- * TRN wizard routes
- */
-
-router.get('/:view', (req, res, next) => {
-  res.locals.paths = trnWizardPaths(req)
-  next()
-})
-
-router.post('/:view', (req, res) => {
-  const fork = trnWizardForks(req)
-  const paths = trnWizardPaths(req)
-  fork ? res.redirect(fork) : res.redirect(paths.next)
-})
+exampleWizardRoutes(router)
+findYourTrnRoutes(router)
 
 export default router
