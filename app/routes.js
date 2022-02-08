f1d3db17d82d3cb7307f27611ea5344ea0f7cb94
Paul Hayes <fofr@users.noreply.github.com>
2022-02-08 17:16:43 +0000

    Use an API for provider autocomplete
    Speed up the page

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { exampleWizardRoutes } from './routes/example-wizard.js'
 import { findYourTrnRoutes } from './routes/find-your-trn.js'
+import { apiRoutes } from './routes/api.js'
 
 const router = express.Router()
 exampleWizardRoutes(router)
 findYourTrnRoutes(router)
+apiRoutes(router)
 
 export default router
