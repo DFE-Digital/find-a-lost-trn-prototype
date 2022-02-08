fe0b8cc4341322cc49551fb7107cfe7489c1b3a9
Paul Hayes <fofr@users.noreply.github.com>
2022-02-08 10:19:36 +0000

    Redirect from old Heroku to new Heroku

--- app/routes.js
+++ app/routes.js
@@ -3,6 +3,12 @@ import { exampleWizardRoutes } from './routes/example-wizard.js'
 import { findYourTrnRoutes } from './routes/find-your-trn.js'
 
 const router = express.Router()
+const newDomain = 'https://find-a-lost-trn.herokuapp.com/'
+
+router.use(function (req, res, next) {
+  return res.redirect(301, newDomain + req.url)
+})
+
 exampleWizardRoutes(router)
 findYourTrnRoutes(router)
 
