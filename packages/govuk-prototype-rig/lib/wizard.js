c8ed694afbab752c671c6d34c0c918b6f77b68a5
Paul Hayes <fofr@users.noreply.github.com>
2022-02-23 11:37:32 +0000

    Bring paths and forks together
    - Simplify the required routes
    - Create a single declarative place for a journey and its forks
    - Export a single wizard function rather than two

--- packages/govuk-prototype-rig/lib/wizard.js
+++ packages/govuk-prototype-rig/lib/wizard.js
@@ -1,29 +1,44 @@
-import _ from 'lodash'
-
 /**
  * @access private
  * @param {Object} req - Express request
  * @returns {string} Original query
  */
-function _originalQuery (req) {
+const originalQuery = (req) => {
   const originalQueryString = req.originalUrl.split('?')[1]
   return originalQueryString ? `?${originalQueryString}` : ''
+}
+
+const getFork = (forks) => {
+  for (const key of Object.keys(forks)) {
+    if (forks[key]()) {
+      return key
+    }
+  }
+  return false
 }
 
 /**
  * Get next, back and current paths in user journey.
  *
- * @param {Array} paths - Sequence of paths in user journey
+ * @param {Object} journey - Sequence of paths in user journey
  * @param {Object} req - Express request
  * @returns {Object} Next and back paths
  */
-export const nextAndBackPaths = (paths, req) => {
-  const currentPath = req.path
-  const query = _originalQuery(req)
+export const wizard = (journey, req) => {
   const data = req.session.data
+  const paths = Object.keys(journey)
+  const currentPath = req.path
+  const query = originalQuery(req)
   const index = paths.indexOf(currentPath)
-  const next = paths[index + 1] || ''
-  let back = paths[index - 1] || ''
+  let fork
+  let next
+  let back
+
+  if (index !== -1) {
+    fork = getFork(journey[currentPath])
+    next = fork || paths[index + 1] || ''
+    back = paths[index - 1] || ''
+  }
 
   // Point back to where we forked from
   if (currentPath === data['forked-to']) {
@@ -36,66 +51,15 @@ export const nextAndBackPaths = (paths, req) => {
     delete data['forked-to']
   }
 
+  // Add a new fork
+  if (fork && req.method === 'POST') {
+    data['forked-from'] = currentPath
+    data['forked-to'] = fork
+  }
+
   return {
-    next: next + query,
-    back: back + query,
+    next: next && next + query,
+    back: back && back + query,
     current: currentPath + query
   }
-}
-
-/**
- * Get next path if at a fork in the user journey.
- *
- * @param {Array} forks - List of forks in user journey
- * @param {Object} req - Express request
- * @returns {Object} Next path in the user journey
- */
-export const nextForkPath = (forks, req) => {
-  const currentPath = req.path
-  const { data } = req.session
-
-  // Detect if current path is a fork
-  const fork = forks.find(obj => obj.currentPath === currentPath)
-
-  if (fork) {
-    if (fork.skipTo) {
-      data['forked-from'] = currentPath
-      data['forked-to'] = fork.skipTo
-
-      return fork.skipTo
-    }
-
-    const storedData = _.get(data, _.toPath(fork.storedData))
-    const storedValues = Array.isArray(storedData) ? storedData : [storedData]
-    const forkPath = (typeof fork.forkPath === 'function' ? fork.forkPath(storedData) : fork.forkPath)
-
-    if (fork.values) {
-      const values = Array.isArray(fork.values) ? fork.values : [fork.values]
-
-      if (values.some(v => storedValues.indexOf(v) >= 0)) {
-        data['forked-from'] = currentPath
-        data['forked-to'] = forkPath
-
-        return forkPath
-      }
-    }
-
-    if (fork.excludedValues) {
-      const excludedValues = Array.isArray(fork.excludedValues) ? fork.excludedValues : [fork.excludedValues]
-
-      if (!excludedValues.some(v => storedValues.indexOf(v) >= 0)) {
-        data['forked-from'] = currentPath
-        data['forked-to'] = forkPath
-
-        return forkPath
-      }
-    }
-  }
-
-  return false
-}
-
-export const wizard = {
-  nextAndBackPaths,
-  nextForkPath
 }
