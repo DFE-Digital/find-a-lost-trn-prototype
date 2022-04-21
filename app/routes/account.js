8c34a0eb49b4280d5766312d3dfaed6f53ac924e
Paul Hayes <fofr@users.noreply.github.com>
2022-04-21 08:42:22 +0100

    Initial create account journey

---
+++ app/routes/account.js
@@ -0,0 +1,42 @@
+import wizard from '../wizards/account.js'
+
+export const accountRoutes = router => {
+  router.all('/account/:view', (req, res, next) => {
+    res.locals.paths = wizard(req)
+    res.locals.accountJourney = true
+    res.locals.serviceName = 'Test service name'
+    next()
+  })
+
+  router.get('/account/email', (req, res) => {
+    res.render('email')
+  })
+
+  router.get('/account/name', (req, res) => {
+    res.render('name')
+  })
+
+  router.get('/account/dob', (req, res) => {
+    res.render('dob')
+  })
+
+  router.get('/account/have-nino', (req, res) => {
+    res.render('have-nino')
+  })
+
+  router.get('/account/nino', (req, res) => {
+    res.render('nino')
+  })
+
+  router.get('/account/have-qts', (req, res) => {
+    res.render('have-qts')
+  })
+
+  router.get('/account/how-qts', (req, res) => {
+    res.render('how-qts')
+  })
+
+  router.post('/account/:view', (req, res) => {
+    res.redirect(res.locals.paths.next)
+  })
+}
---
+++ app/wizards/account.js
@@ -0,0 +1,46 @@
+import { wizard } from 'govuk-prototype-rig'
+import { userMatchesDQTRecord } from '../utils/dqt.js'
+
+function emailHasAccount (data) {
+  return false
+}
+
+export default (req) => {
+  const data = req.session.data
+  const journey = {
+    '/account/email': {
+      '/account/sign-in-with-password': () => emailHasAccount(data)
+    },
+    '/account/email-confirmation': {},
+    '/account/trn-holder': {
+      '/account/name': { data: 'account.do-you-have-a-trn', value: 'No' }
+    },
+    '/account/trn-known': {
+      '/account/name': { data: 'account.do-you-know-your-trn', value: 'no' }
+    },
+    '/account/trn': {},
+    '/account/name': {},
+    '/account/dob': {
+      '/account/create-account': () => userMatchesDQTRecord(data)
+    },
+    '/account/have-nino': {
+      '/account/have-qts': { data: 'have-nino', value: 'No' }
+    },
+    '/account/nino': {
+      '/account/create-account': () => userMatchesDQTRecord(data)
+    },
+    '/account/have-qts': {
+      '/account/create-account': { data: 'has-qts', value: 'No' }
+    },
+    '/account/how-qts': {
+      '/account/no-match': () => !userMatchesDQTRecord(data)
+    },
+    '/account/create-account': {
+      '/account/return-to-service': { data: 'create-account', value: 'No' }
+    },
+    '/account/create-password': {},
+    '/account/return-to-service': {}
+  }
+
+  return wizard(journey, req)
+}
