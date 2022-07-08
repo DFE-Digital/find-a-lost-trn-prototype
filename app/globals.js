b6bcfd913e6c07462042b290a86c96e916b8161f
Paul Hayes <fofr@users.noreply.github.com>
2022-07-08 14:01:41 +0100

    Add summary list helpers

--- app/globals.js
+++ app/globals.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import providers from '../app/data/reference/itt-providers.js'
 
 /**
@@ -26,6 +27,87 @@ export default () => {
 
   globals.ittProviderOptionNames = function () {
     return providers.map(name => { return { text: name } })
+  }
+
+  globals.d = function (keyPath) {
+    const data = this.ctx.data
+    return _.get(data, _.toPath(keyPath))
+  }
+
+  const evaluateCondition = (data, condition) => {
+    if (typeof condition === 'function' && condition()) {
+      return true
+    }
+
+    if (typeof condition === 'string') {
+      return !!_.get(data, _.toPath(condition))
+    }
+
+    if (typeof condition === 'object' && condition.data) {
+      const sessionData = _.toPath(_.get(data, condition.data))
+
+      if (condition.value || condition.values) {
+        const includedValues = _.toPath(condition.value ? condition.value : condition.values)
+        if (includedValues.some(v => sessionData.indexOf(v) >= 0)) {
+          return true
+        }
+      }
+
+      if (condition.excludedValue || condition.excludedValues) {
+        const excludedValues = _.toPath(condition.excludedValue ? condition.excludedValue : condition.excludedValues)
+        if (!excludedValues.some(v => sessionData.indexOf(v) >= 0)) {
+          return true
+        }
+      }
+    }
+
+    return false
+  }
+
+  globals.decorateRows = function (rows) {
+    const data = this.ctx.data
+
+    rows = rows
+      .filter(r => typeof r.condition === 'undefined' || evaluateCondition(data, r.condition))
+      .map(r => {
+        if (typeof r.key === 'string') {
+          if (!r.visuallyHiddenText) {
+            r.visuallyHiddenText = r.key.toLowerCase()
+          }
+
+          r.key = {
+            text: r.key
+          }
+        }
+
+        if (typeof r.data === 'string' && !r.value) {
+          r.value = _.get(data, _.toPath(r.data))
+          delete r.data
+        }
+
+        if (typeof r.value === 'string') {
+          r.value = {
+            text: r.value
+          }
+        }
+
+        if (r.href && !r.actions) {
+          r.actions = {
+            items: [{
+              text: r.action || 'Change',
+              visuallyHiddenText: r.visuallyHiddenText,
+              href: r.href
+            }]
+          }
+
+          delete r.visuallyHiddenText
+          delete r.href
+        }
+
+        return r
+      })
+
+    return rows
   }
 
   // Keep the following line to return your globals to the app
