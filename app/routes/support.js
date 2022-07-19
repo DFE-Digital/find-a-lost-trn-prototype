4118f5d402c2e2e8867b073071fd53756f5dd1ee
Paul Hayes <fofr@users.noreply.github.com>
2022-07-19 11:14:08 +0100

    Add a name change wizard
    Allow users to save and change names

--- app/routes/support.js
+++ app/routes/support.js
@@ -1,3 +1,5 @@
+import supportNameChangeWizard from '../wizards/support-name-change-wizard.js'
+
 export const supportRoutes = router => {
   router.all(['/support', '/support/*'], (req, res, next) => {
     res.locals.serviceName = 'Support for Get an identity'
@@ -6,14 +8,44 @@ export const supportRoutes = router => {
 
   router.all(['/support/:user', '/support/:user/*'], (req, res, next) => {
     res.locals.user = req.session.data.support.users[req.params.user]
+    res.locals.dataPath = `support.users.${req.params.user}`
+    res.locals.urlPath = `/support/${req.params.user}`
     next()
   })
 
+  router.all(['/support/:user/name-change', '/support/:user/name-change/*'], (req, res, next) => {
+    res.locals.paths = supportNameChangeWizard(req, res)
+    next()
+  })
+
+  router.post('/support/:user/*', (req, res, next) => {
+    if (res.locals.paths) {
+      res.redirect(res.locals.paths.next)
+    } else {
+      next()
+    }
+  })
+
   router.all('/support/:user', (req, res) => {
+    if (req.query.success) {
+      switch (req.query.success) {
+        case 'name-change':
+          res.locals.appSuccess = { heading: 'Name changed successfully' }
+          break
+        default:
+          res.locals.appSuccess = { heading: 'Changes saved' }
+          break
+      }
+    }
+
     res.render('support/user')
   })
 
   router.all('/support/:user/:view', (req, res) => {
     res.render(`support/${req.params.view}`)
+  })
+
+  router.all('/support/:user/:folder/:view', (req, res) => {
+    res.render(`support/${req.params.folder}/${req.params.view}`)
   })
 }
