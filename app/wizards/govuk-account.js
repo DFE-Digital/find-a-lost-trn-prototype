89197039daf04fd290bc9ece839b50be3cf98893
Paul Hayes <fofr@users.noreply.github.com>
2022-09-02 17:02:15 +0100

    Add GOVUK account journey with phone

--- app/wizards/govuk-account.js
+++ app/wizards/govuk-account.js
@@ -2,7 +2,14 @@ import { wizard } from 'govuk-prototype-rig'
 
 export default (req) => {
   const journey = {
-
+    '/govuk-account/sign-in-or-create': {},
+    '/govuk-account/enter-email-create': {},
+    '/govuk-account/check-email': {},
+    '/govuk-account/create-password': {},
+    '/govuk-account/get-security-codes': {},
+    '/govuk-account/enter-phone-number': {},
+    '/govuk-account/check-your-phone': {},
+    '/govuk-account/account-created': {}
   }
 
   return wizard(journey, req)
