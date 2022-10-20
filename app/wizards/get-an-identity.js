19d3b44a1b6480e5ce7a72752a1c86dc9fbb6315
Paul Hayes <fofr@users.noreply.github.com>
2022-10-20 11:26:48 +0100

    Fix return to service link

--- app/wizards/get-an-identity.js
+++ app/wizards/get-an-identity.js
@@ -18,9 +18,8 @@ export default (req) => {
       '/get-an-identity/ask-questions': () => !emailHasIdentity(data)
     },
     '/get-an-identity/signed-in-as': {
-      '/get-an-identity/edit-details': {}
+      '/get-an-identity/return-to-service': true
     },
-    '/get-an-identity/edit-details': {},
     '/get-an-identity/ask-questions': {},
     ...!trnRequired
       ? {
