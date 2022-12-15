c8db4ac63046b2fb7f3f4068f202ddccd19d16fc
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2022-12-15 09:29:09 +0000

    Return user journey

--- app/wizards/get-an-identity-update-details.js
+++ app/wizards/get-an-identity-update-details.js
@@ -2,10 +2,8 @@ import { wizard } from 'govuk-prototype-rig'
 
 export default (req) => {
   const journey = {
-    '/get-an-identity/check-answer-npq': {},
-      '/get-an-identity/account-details': {},
     '/get-an-identity/edit-preferred-name': {
-      '/get-an-identity/account-details?success=preferred-name': true
+      '/get-an-identity/signed-in-as?success=preferred-name': true
     }
   }
   return wizard(journey, req)
--- app/wizards/get-an-identity-update-email.js
+++ app/wizards/get-an-identity-update-email.js
@@ -5,26 +5,13 @@ export default (req) => {
   
   const newAuth = data.features.newAuth.on
   const journey = {
-        ...(newAuth)
-    ? {
-      '/get-an-identity/check-answer-npq': {},
-      '/get-an-identity/account-details': {},
-      '/get-an-identity/change-email': {},
-  
-      '/get-an-identity/email-confirmation2': {
-  
-        '/get-an-identity/account-details?success=email-update': true
-      }
+
+    '/get-an-identity/change-email': {},
+
+    '/get-an-identity/email-confirmation2': {
+
+      '/get-an-identity/signed-in-as?success=email-update': true
     }
-    : {},
-    // '/get-an-identity/signed-in-as': {},
-    // '/get-an-identity/edit-details': {},
-    // '/get-an-identity/change-email': {},
-
-    // '/get-an-identity/email-confirmation2': {
-
-    //   '/get-an-identity/signed-in-as?success=email-update': true
-    // }
   }
   return wizard(journey, req)
 }
