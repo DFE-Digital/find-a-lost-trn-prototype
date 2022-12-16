32b53df3ff9d607b8d86e19d0214dc4a66e9f983
tarikjohnston1 <108064159+tarikjohnston2@users.noreply.github.com>
2022-12-16 13:29:54 +0000

    Update to files

--- app/wizards/get-an-identity-update-name.js
+++ app/wizards/get-an-identity-update-name.js
@@ -2,10 +2,8 @@ import { wizard } from 'govuk-prototype-rig'
 
 export default (req) => {
   const journey = {
-    '/get-an-identity/check-answer-npq': {},
-      '/get-an-identity/account-details': {},
     '/get-an-identity/edit-name': {
-      '/get-an-identity/account-details?success=name': true
+      '/get-an-identity/signed-in-as?success=name': true
     }
   }
   return wizard(journey, req)
